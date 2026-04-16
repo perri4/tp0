cb7bcf686bddd1de4dbed5887fdb5fa7464a71b0
perri4 <jmarquez@frba.utn.edu.ar>
2026-04-16 19:53:06 +0000

    mejoramos lass cositas :)

--- server/src/utils.c
+++ server/src/utils.c
@@ -5,33 +5,35 @@ t_log* logger;
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	// assert(!"no implementado!");
+	assert(!"no implementado!");
 
 	int socket_servidor;
 
-	int err; 
+	//declara las "variables" que van a ser de tipo addrinfo (una estructura)
+	//por ejemplo; "hints" es una instancia literal de la estructura addrinfo, con todos sus campos correspondientes.
+	//mientras que "*servinfo" y "*p" son punteros a instancias particulares de addrinfo.
+	struct addrinfo hints, *servinfo, *p; 
 
-	struct addrinfo hints, *servinfo, *p;
+	memset(&hints, 0, sizeof(hints)); //aca esta liberando el espacio de memoria donde 
+		// esta almacenado "hints", para que no contenga pavadas y llene sus campos con cosas falopa. 
 
-	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	err = getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	socket_servidor = getaddrinfo(NULL, PUERTO, &hints, &servinfo);
 
 	// Creamos el socket de escucha del servidor
-	socket_servidor = socket(servinfo->ai_family,
-							servinfo->ai_socktype,
-							servinfo->ai_protocol);
+	int socket_escucha = socket(servinfo->ai_family,
+						servinfo->ai_socktype,
+						servinfo->ai_protocol);
 
-	err = setsockopt(socket_servidor, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
-	
 	// Asociamos el socket a un puerto
-	err = bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
-	
+	socket_servidor = bind(socket_escucha, servinfo->ai_addr, servinfo->ai_addrlen);
+
 	// Escuchamos las conexiones entrantes
-	err = listen(socket_servidor, SOMAXCONN);
+	socket_servidor = listen(socket_escucha, SOMAXCONN);
+
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -45,10 +47,7 @@ int esperar_cliente(int socket_servidor)
 	assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente; //duda.., esto es un "fd_conexion" ??
-	
-	socket_cliente = accept(socket_servidor, NULL, NULL);
-	
+	int socket_cliente;
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
